abc4d3f0fb9f77e9f7497fc23c684668fdb1e9ac
rizanoor <rizanoorfauzan123@gmail.com>
2024-05-24 15:28:40 +0700

    feat : add range age

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import cv2
 
+# Load face detection model
 faceRef = cv2.CascadeClassifier("faceRef.xml")
+
+# Load age detection model
+ageNet = cv2.dnn.readNetFromCaffe("deploy_age.prototxt", "age_net.caffemodel")
+
+# Define age ranges
+ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+
 camera = cv2.VideoCapture(0)
 
 def faceDetection(frame):
     optimizeFrame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-    faces = faceRef.detectMultiScale(optimizeFrame, scaleFactor=1.1,
-                                     minSize=(500, 500))
+    faces = faceRef.detectMultiScale(optimizeFrame, scaleFactor=1.1, minSize=(500, 500))
     return faces
 
-def drawerBox(frame):
-    for x, y, w, h, in faceDetection(frame):
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
-        cv2.putText(frame, 'Riza', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+def agePrediction(face):
+    blob = cv2.dnn.blobFromImage(face, 1.0, (227, 227), (78.4263377603, 87.7689143744, 114.895847746), swapRB=False)
+    ageNet.setInput(blob)
+    agePreds = ageNet.forward()
+    age = ageList[agePreds[0].argmax()]
+    return age
 
-    
+def drawerBox(frame):
+    for x, y, w, h in faceDetection(frame):
+        face = frame[y:y+h, x:x+w]
+        age = agePrediction(face)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 4)
+        cv2.putText(frame, f'Riza, {age}', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
 def closeWindow():
     camera.release()
     cv2.destroyAllWindows()
